ced88845f8989d332cf2aa41e14fd90ce981f10b
Kamil Janas <127615923+Yaynas@users.noreply.github.com>
2023-04-11 14:35:26 +0200

    Study-11042023

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,33 @@
 ﻿using System.Windows;
 using System.Windows.Input;
 using ShoppingApp.Core;
+using ShoppingApp.Core.Services;
 
 namespace ShoppingApp
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IMessageService
     {
+        private MainWindow mainWindowInstance;
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel(this);
+
+            mainWindowInstance = (MainWindow)Application.Current.MainWindow;
         }
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
         }
 
-        public void MinimizeApplicationMainWindow()
+
+        public void CloseApplication()
+        {
+            App.Current.Shutdown();
+        }
+
+        public void MinimalizeApplication()
         {
             WindowState = WindowState.Minimized;
         }
