a3fc63693833c1dd961a78c11fc4ec9f7db653b0
Kamil Janas <127615923+Yaynas@users.noreply.github.com>
2023-04-23 15:51:34 +0200

    Study-23042023

--- App.xaml.cs
+++ App.xaml.cs
@@ -5,10 +5,22 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using ShoppingApp.Database;
+using ShoppingApp.Core;
 
 namespace ShoppingApp
 {
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            var database = new ShoppingListDBContext();
+
+            database.Database.EnsureCreated();
+
+            DatabaseCreationTool.MyDatabase = database;
+        }
     }
 }
